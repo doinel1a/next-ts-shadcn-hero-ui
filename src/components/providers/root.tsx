34ba993eacfa0ee3e47d5efa589ff8e8d6e58632
doinel1a <doinel1atanasiu@gmail.com>
2025-05-08 22:45:00 +0200

    refactor: move client providers into client folder

--- src/components/providers/root.tsx
+++ src/components/providers/root.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import type { PropsWithChildren } from 'react';
 
-import HeroUiProvider from './hero-ui';
-import TanstackQueryProvider from './tanstack-query';
-import ThemeProvider from './theme';
+import HeroUiProvider from './client/hero-ui';
+import TanstackQueryProvider from './client/tanstack-query';
+import ThemeProvider from './client/theme';
 
 type TRootProvider = PropsWithChildren;
 

c6b90719a170b793493aaf30fa3566b2baa91f00
doinel1a <doinel1atanasiu@gmail.com>
2026-01-03 21:16:25 +0100

    fix: eslint errors & warnings

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import React from 'react';
-
 import { Button } from '@heroui/button';
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@heroui/dropdown';
 import { IconDeviceLaptop, IconMoonStars, IconSun } from '@tabler/icons-react';

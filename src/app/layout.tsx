15562d618709bc34d9ec234d3fd57f60b6893b0b
doinel1a <doinel1atanasiu@gmail.com>
2026-01-03 21:11:28 +0100

    refactor: theme colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,23 @@ import type { Metadata, Viewport } from 'next';
 import type { PropsWithChildren } from 'react';
 
 import config from '_config';
+import { Geist, Geist_Mono, Nunito_Sans } from 'next/font/google';
 
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import RootProvider from '@/components/providers/root';
+
+const nunitoSans = Nunito_Sans({ variable: '--font-sans' });
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+});
 
 export const metadata: Metadata = {
   title: config.metadata.title,
@@ -28,8 +41,8 @@ type TRootLayout = PropsWithChildren;
 
 export default function RootLayout({ children }: Readonly<TRootLayout>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body>
+    <html lang='en' className={nunitoSans.variable} suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <RootProvider>
           <div className='grid min-h-dvh grid-rows-[auto_1fr_auto]'>
             <Navbar />
